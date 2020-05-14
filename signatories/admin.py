14638bb317c4c4b3e86bfe71aba52accd1f5bf44
Antonin Delpeuch <antonin@delpeuch.eu>
2020-05-14 11:58:05 +0200

    Raw id field for signatories admin

--- signatories/admin.py
+++ signatories/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Signatory
 
-admin.site.register(Signatory)
+class SignatoryAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)
+
+admin.site.register(Signatory, SignatoryAdmin)
